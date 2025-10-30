2c62c8cc35a5842d8a0026530ec39e41c9b41dd6
Elena Cuevas de la Hera <elena.cuevas@vasscompany.com>
2025-10-30 17:56:35 +0100

    add manifest and final fixes

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -23,13 +23,56 @@ export class App extends LitElement {
         }
 
         img {
-            width: 400px;
+            width: 350px;
             margin-top: 40px;
         }
 
         .container {
             padding: 40px;
             background-color: #fff;
+            border-radius: 16px;
+        }
+
+        #set-time {
+            margin-top: 32px;  
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+            font-family: sans-serif;
+        }
+
+        input[type="number"] {
+            all: unset;
+            border-bottom: 1px solid #1c1c1c;
+            text-align: center;
+        }
+
+        .set-time--input {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-bottom: 10px;
+            width: 100%;
+            flex-wrap: wrap;
+        }
+
+        .set-time--input span {
+            margin: 10px;
+        }
+
+        button {
+            padding: 0.6rem 1.2rem;
+            font-size: 1rem;
+            cursor: pointer;
+            border: none;
+            border-radius: 0.3rem;
+            color: #1c1c1c;
+            background-color: #e5e5e5;
+        }
+
+        button:hover {
+            background-color: #cfcfcfff;
         }
     `;
 
@@ -38,7 +81,7 @@ export class App extends LitElement {
         const timerPlayer = this.shadowRoot.querySelector('timer-component');
         this.start = Number(timeValue);
         setTimeout(() => {
-            timerPlayer.reset();
+            timerPlayer.resetTimer();
         }, 100);
     }
 
@@ -51,7 +94,7 @@ export class App extends LitElement {
                     <sound-component sound="${this.sounnd}" id="finish-sound"></sound-component>
                 </div>
                 <div id="set-time">
-                    <div id="set-time--input">
+                    <div class="set-time--input">
                         <span>Configurar</span>
                         <input type="number" id="time" value="${this.start}">
                         <span>segundos</span>
